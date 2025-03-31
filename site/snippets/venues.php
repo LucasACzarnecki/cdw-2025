4c7e53d889d3abca6abda7df433d1d30f17eed94
Lucas Czarnecki <me@lucasczarnecki.com>
2025-03-31 19:59:06 -0400

    Layout tweak

--- site/snippets/venues.php
+++ site/snippets/venues.php
@@ -1,11 +1,11 @@
 <?php if (!empty($venues)): ?>
     <section id="venues" class="mb-10 grid grid-four gap-1 torn-paper purple-block cta-block">
     <h2><?= count($venues) > 1 ? 'Venues' : 'Venue' ?></h2>
-        <div class="grid grid-three gap-2 span-3">
+        <div class="grid grid-two gap-1 span-3">
             <?php foreach ($venues as $venueName): ?>
                 <?php $venue = site()->venues()->toStructure()->filterBy('name', $venueName)->first(); ?>
                 <?php if ($venue): ?>
-                    <div class="span-2">
+                    <div class="">
                     <?php if ($venue->image()->isNotEmpty() && $file = $venue->image()->toFile()): ?>
                             <img src="<?= $file->crop(1200,800)->url() ?>" alt="<?= $venue->name() ?>">
                         <?php endif ?>
